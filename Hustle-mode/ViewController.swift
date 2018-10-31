3b06f6f5574bd1f92e73b1fad22246d8bd85be35
Mark Morales <marktravismorales@gmail.com>
2018-10-30 20:15:49 -0500

    Initial Commit

---
+++ Hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hustle-mode
+//
+//  Created by Mark Morales on 10/30/18.
+//  Copyright © 2018 Mark Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
